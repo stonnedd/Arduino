99d3e39008c0c8d9f7e5a386d1f8db2cc39d670f
SnD <stonnedd@hotmail.com>
2013-08-15 18:44:19 -0500

    first step arduino client c#

---
+++ ArduinoClient/ArduinoClient/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace ArduinoClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            SocketClient();
+        }
+
+        private void SocketClient()
+        {
+            var bytes = new byte[1024];
+            var adreess = new IPEndPoint(IPAddress.Parse("192.168.2.32"), 23);
+            var sender = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                sender.Connect(adreess);
+
+                MessageBox.Show("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                // Encode the data string into a byte array.
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                // Send the data through the socket.
+                int bytesSent = sender.Send(msg);
+
+                // Receive the response from the remote device.
+                int bytesRec = sender.Receive(bytes);
+                MessageBox.Show("Echoed test = {0}",
+                    Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Release the socket.
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                MessageBox.Show("ArgumentNullException : {0}", ane.ToString());
+            }
+
+
+        }
+
+
+    }
+}
